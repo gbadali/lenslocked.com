e1de91a9d651f73b9f025bbba1465dc21184c2b6
Stephen <gbadali@gmail.com>
2019-03-11 15:07:16 -0500

    8.4.2 Setting up  GORM

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,42 +16,20 @@ const (
 	dbname   = "lenselocked_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("succesfully connected!")
 	defer db.Close()
-
-	var id int
-	for i := 1; i < 6; i++ {
-		// create somoe fake data
-		userID := 1
-		if i > 3 {
-			userID = 2
-		}
-		amount := 1000 * i
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-
-		err = db.QueryRow(`
-			INSERT INTO orders (user_id, amount, description)
-			VALUES ($1, $2, $3)
-			RETURNING id`,
-			userID, amount, description).Scan(&id)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Created an ordder with the ID:", id)
-	}
 }
