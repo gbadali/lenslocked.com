f9579f61acaf7499b81e486ae1e1004069db39fc
Stephen <gbadali@gmail.com>
2019-03-11 15:48:46 -0500

    8.4.8 Creating related moels with GORM

--- exp/main.go
+++ exp/main.go
@@ -25,29 +25,35 @@ type User struct {
 	Email string `gorm:"not null;unique_index"`
 }
 
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-
 	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
 	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Order{})
 
-	db.AutoMigrate(&User{})
+	var user User
+	db.First(&user)
+	if db.Error != nil {
+		panic(db.Error)
+	}
 
-	name, email := getInfo()
-	u := &User{
-		Name:  name,
-		Email: email,
-	}
-	if err = db.Create(u).Error; err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v\n", u)
+	createOrder(db, user, 1001, "Fake Description #1")
+	createOrder(db, user, 9999, "Fake Description #2")
+	createOrder(db, user, 8800, "Fake Description #3")
 }
 
 func getInfo() (name, email string) {
@@ -58,4 +64,15 @@ func getInfo() (name, email string) {
 	email, _ = reader.ReadString('\n')
 	email = strings.TrimSpace(email)
 	return name, email
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
 }
