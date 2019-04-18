9ac2c50b6c80c7cc214a294494bbc3dcb8fdf19e
Stephen <gbadali@gmail.com>
2019-04-17 18:20:26 -0700

    11.7.5

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gbadali/lenslocked.com/models"
-
-	"github.com/gorilla/mux"
-
-	"github.com/gbadali/lenslocked.com/controllers"
 )
 
 const (
@@ -19,48 +14,84 @@ const (
 	dbname   = "lenslocked_dev"
 )
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound) // StatusNotFound = 404
-	fmt.Fprint(w, "Couldn't find page 404&#128169")
-}
-
 func main() {
-	// Create a DB connection string and then use it to
-	// create our model services.
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-
 	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer us.Close()
-	us.AutoMigrate()
-	// Do a destructive reset on the DB for schema changes we can't migrate
-	// us.DestructiveReset()
+	us.DestructiveReset()
 
-	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers(us)
-
-	r := mux.NewRouter()
-	r.Handle("/", staticC.Home).Methods("GET")
-	r.Handle("/contact", staticC.Contact).Methods("GET")
-	r.Handle("/faq", staticC.FAQ).Methods("GET")
-	r.HandleFunc("/signup", usersC.New).Methods("GET")
-	r.HandleFunc("/signup", usersC.Create).Methods("POST")
-	r.Handle("/login", usersC.LoginView).Methods("GET")
-	r.HandleFunc("/login", usersC.Login).Methods("POST")
-	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
-
-	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-
-	http.ListenAndServe(":3000", r)
-}
-
-func must(err error) {
+	user := models.User{
+		Name:     "Michael Scott",
+		Email:    "michael@dundermifflin.com",
+		Password: "bestboss",
+	}
+	err = us.Create(&user)
 	if err != nil {
 		panic(err)
 	}
+	// Verify that the user has a Remember and RememberHash
+	fmt.Printf("%+v\n", user)
+	if user.Remember == "" {
+		panic("Invalid remember token")
+	}
+
+	// Now verify that we can lookup the user with that remember
+	// token
+	user2, err := us.ByRem
+	ember(user.Remember)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", *user2)
 }
+
+// func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html")
+// 	w.WriteHeader(http.StatusNotFound) // StatusNotFound = 404
+// 	fmt.Fprint(w, "Couldn't find page 404&#128169")
+// }
+
+// func main() {
+// 	// Create a DB connection string and then use it to
+// 	// create our model services.
+// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
+
+// 	us, err := models.NewUserService(psqlInfo)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer us.Close()
+// 	us.AutoMigrate()
+// 	// Do a destructive reset on the DB for schema changes we can't migrate
+// 	// us.DestructiveReset()
+
+// 	staticC := controllers.NewStatic()
+// 	usersC := controllers.NewUsers(us)
+
+// 	r := mux.NewRouter()
+// 	r.Handle("/", staticC.Home).Methods("GET")
+// 	r.Handle("/contact", staticC.Contact).Methods("GET")
+// 	r.Handle("/faq", staticC.FAQ).Methods("GET")
+// 	r.HandleFunc("/signup", usersC.New).Methods("GET")
+// 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+// 	r.Handle("/login", usersC.LoginView).Methods("GET")
+// 	r.HandleFunc("/login", usersC.Login).Methods("POST")
+// 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
+
+// 	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+// 	http.ListenAndServe(":3000", r)
+// }
+
+// func must(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
