29f46a17b5cd00c89d69861e98993d43fdb5bfb8
Stephen <gbadali@gmail.com>
2019-05-24 04:39:20 -0700

    14.5.1 starting to flesh out the galleries service

--- models/galleries.go
+++ models/galleries.go
@@ -6,19 +6,42 @@ import (
 
 type Gallery struct {
 	gorm.Model
-	UserID uint   `gorm:"not_null;index`
-	Title  string `gorm:"not_null`
+	// a unique id for each user
+	UserID uint `gorm:"not_null;index`
+	// the title of each gallery
+	Title string `gorm:"not_null`
 }
-
 type GalleryService interface {
+	GalleryDB
+}
+type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
+
+type galleryService struct {
+	GalleryDB
+}
+type GalleryValidator struct {
+	GalleryDB
+}
+
+// check to make sure galleryGorm impements the GalleryDB methods
+var _ GalleryDB = &galleryGorm{}
 
 type galleryGorm struct {
 	db *gorm.DB
 }
 
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &GalleryValidator{
+			GalleryDB: &galleryGorm{
+				db: db,
+			},
+		},
+	}
+}
+
 func (gg *galleryGorm) Create(gallery *Gallery) error {
-	// TODO: Implement this later
-	return nil
+	return gg.db.Create(gallery).Error
 }
