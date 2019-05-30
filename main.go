91998a668f80d9c9dada946d4a1b36af4d63723d
Stephen <gbadali@gmail.com>
2019-05-30 05:30:28 -0700

    14.8.1 Editing galleries

--- main.go
+++ main.go
@@ -65,7 +65,8 @@ func main() {
 	r.HandleFunc("/galleries", createGallery).Methods("POST")
 	r.HandleFunc("/galleries/{id:[0-9]+}",
 		galleriesC.Show).Methods("GET").Name(controllers.ShowGallery)
-
+	r.HandleFunc("/galleries/{id:[0-9]+}/edit",
+		requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET")
 	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	fmt.Println("Starting the server on :3000...")
